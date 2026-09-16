19d2fab3af0a7d78bf64af108e1a0b7409594102
FAlvarez19Laptop <f.alvarezarg@gmail.com>
2026-09-16 15:42:52 -0300

    cambiando

--- SisPedidos/wsgi.py
+++ SisPedidos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PedidosSM project.
+WSGI config for STERAKFOOD project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PedidosSM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SisPedidos.settings')
 
 application = get_wsgi_application()
