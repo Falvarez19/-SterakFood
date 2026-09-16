c6e642c7977913ebd0e3f9ae094bf1f4d73d88f5
FAlvarez19Laptop <f.alvarezarg@gmail.com>
2026-09-16 15:59:01 -0300

    subiendo cambios botoneras

--- pedidos/static/script/script.js
+++ pedidos/static/script/script.js
@@ -241,7 +241,7 @@ function validarYEnviar(event) {
     Swal.fire({
         title: 'Procesando...',
         text: '¡Llevando el pedido a toda velocidad!',
-        imageUrl: '/static/img/corriendo.gif',
+        imageUrl: '/static/img/wok.gif',
         imageWidth: 120,
         showConfirmButton: false,
         allowOutsideClick: false, 
